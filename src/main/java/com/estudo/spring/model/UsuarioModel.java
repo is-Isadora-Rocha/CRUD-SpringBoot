c1d3b043fac1dd71b633ec019b06f73c40426fd5
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-03-18 11:53:05 -0300

    init commit

---
+++ src/main/java/com/estudo/spring/model/UsuarioModel.java
@@ -0,0 +1,12 @@
+package com.estudo.spring.model;
+//representação da tabela do banco no java.
+
+import java.util.Date;
+
+public class UsuarioModel {
+
+    private String login;
+    private String senha;
+    private Date dataCadastro;
+}
+
